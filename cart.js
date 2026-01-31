c878ec2d9a6509283a259df6a240caff10756bf2
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-01-31 17:47:39 -0300

    Remove unused methods

--- cart.js
+++ cart.js
@@ -18,22 +18,8 @@ export const Cart = (function () {
     return currentItem[0];
   }
 
-  function updateItem(name) {
-    // search for the item and update its quantity and totalPrice
-    const currentItem = cartItems.filter(function (item) {
-      return (item.name = name);
-    });
-
-    const quantity = DessertsInfo.getQauntity(dessertIndex);
-
-    currentItem.quantity.textContent = `${quantity}x`;
-
-    currentItem.totalPrice.textContent = `\$${(quantity * currentItemprice).toFixed(2)}`;
-  }
-
   return {
     addItem,
-    updateItem,
     getItem,
   };
 })();
